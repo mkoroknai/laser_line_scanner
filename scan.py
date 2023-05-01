116146b8e4973f49b610bccdf4f5670930a1977e
Mate <moroknai@gmail.com>
2023-05-02 00:06:11 +0200

    add file that will be the main script

---
+++ scan.py
@@ -0,0 +1,17 @@
+import math
+from laser_scanner import LaserScanner
+
+
+THRESHOLD_MIN = 254
+THRESHOLD_MAX = 255
+
+CAM_FOV = 64.8 # deg
+D_CL = 0.14 # distance between camera and laser position, in m
+ALPHA_LASER = 17 # laser tilted by this much, degrees
+
+laser_scanner = LaserScanner(camFoV=math.radians(CAM_FOV), distCamera2Laser=D_CL,
+                             alphaLaser=math.radians(ALPHA_LASER), camId=0)
+
+
+laser_scanner.run()
+laser_scanner.close()
